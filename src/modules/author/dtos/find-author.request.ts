5fdd612088eaaf0c5ef15217f482e7f48ec4ad41
Caio Braga <caioaugustobrg@gmail.com>
2023-12-02 23:31:08 -0300

    :package: feat: dtos

--- src/modules/author/dtos/find-author.request.ts
+++ src/modules/author/dtos/find-author.request.ts
@@ -1,3 +1,9 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+import { Book } from 'src/modules/book/models/book.model';
+
 export class FindAuthorDTO {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+  book?: Book[];
 }
