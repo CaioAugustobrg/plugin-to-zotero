e5ad8ecb6a592c94344c4a1d34f1f355dabb25fe
Caio Braga <caioaugustobrg@gmail.com>
2023-12-02 23:29:45 -0300

    :sparkles: feat: author service

--- src/modules/author/services/author.service.ts
+++ src/modules/author/services/author.service.ts
@@ -4,27 +4,24 @@ import { PrismaAuthorRepository } from '../repositories/prisma-author.repository
 import { FindAuthorDTO } from '../dtos/find-author.request';
 import { type HttpRequest, type HttpResponse } from 'src/ports/http';
 import ApiError from 'src/utils/apiError';
-interface TypedRequest<T> extends HttpRequest {
-  body: T;
-}
+
 @Injectable()
 export class AuthorService {
   constructor(
     private readonly prismaAuthorRepository: PrismaAuthorRepository,
   ) {}
 
-  async handle(
-    HttpRequest: TypedRequest<FindAuthorDTO>,
-  ): Promise<Author | HttpResponse> {
-    const { name } = HttpRequest.body;
-
-    const findAuthor = await this.prismaAuthorRepository.findAuthorByName(name);
+  async handle(httpRequest: FindAuthorDTO): Promise<Author | HttpResponse> {
+    const findAuthor = await this.prismaAuthorRepository.findAuthorByName(
+      httpRequest.name,
+    );
     if (!findAuthor) {
       throw new ApiError({
         code: 404,
         message: 'Nenhum autor encontrado',
       });
     }
-    return findAuthor;
+    console.log('service', findAuthor);
+    return findAuthor as Author;
   }
 }
