9a2442a0f5267f6287a7dbc9a8cbeef13c3db237
Caio Braga <caioaugustobrg@gmail.com>
2023-12-02 23:29:59 -0300

    :sparkles: feat: app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
+// app.module.ts
+
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PrismaService } from './prisma.service'; // Importe o PrismaService aqui
+import { AuthorModule } from './modules/author/author.module';
+import { PrismaAuthorRepository } from './modules/author/repositories/prisma-author.repository';
+import { AuthorRepository } from './modules/author/repositories/author.repository';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthorModule],
+  providers: [
+    PrismaService, // Adicione o PrismaService aos provedores do AppModule
+    {
+      provide: AuthorRepository,
+      useClass: PrismaAuthorRepository,
+    },
+    // Outros provedores e serviços aqui...
+  ],
 })
 export class AppModule {}
